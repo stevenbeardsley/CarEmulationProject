217a00dccd1f7db59cb3582bf8ff243c8cc0f4bd
Steven Beardsley <swbeardsley2004@gmail.com>
2025-09-27 13:01:22 +0100

    Three blank pages

--- SimulationPlatform/SimulationPlatform/MainWindow.xaml.cs
+++ SimulationPlatform/SimulationPlatform/MainWindow.xaml.cs
@@ -1,29 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using SimulationPlatform.Pages;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace SimulationPlatform;
-/// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
-/// </summary>
-public sealed partial class MainWindow : Window
+namespace SimulationPlatform
 {
-    public MainWindow()
+    public sealed partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            // Navigate to default page on startup
+            ContentFrame.Navigate(typeof(DeployPage));
+            NavView.SelectedItem = NavView.MenuItems[0];
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender,
+                                              NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.SelectedItem is NavigationViewItem item)
+            {
+                switch (item.Tag)
+                {
+                    case "DeployPage":
+                        ContentFrame.Navigate(typeof(DeployPage));
+                        break;
+                    //case "TrackPage":
+                    //    ContentFrame.Navigate(typeof(TrackPage));
+                    //    break;
+                    //case "LoggingPage":
+                    //    ContentFrame.Navigate(typeof(LoggingPage));
+                    //    break;
+                }
+            }
+        }
     }
 }
---
+++ SimulationPlatform/SimulationPlatform/Pages/DeployPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace SimulationPlatform.Pages
+{
+    public sealed partial class DeployPage : Page
+    {
+        public DeployPage()
+        {
+            this.InitializeComponent();
+            // Set a simple title in the TextBlock defined in XAML
+        }
+    }
+}
