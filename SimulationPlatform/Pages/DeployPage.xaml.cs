3348ef7dc131efb92ae0740afcd3b0c1fa130012
Steven Beardsley <swbeardsley2004@gmail.com>
2025-11-05 17:17:57 +0000

    Moving logging to the logging page

--- SimulationPlatform/Pages/DeployPage.xaml.cs
+++ SimulationPlatform/Pages/DeployPage.xaml.cs
@@ -9,16 +9,11 @@ namespace SimulationPlatform.Pages
     public sealed partial class DeployPage : Page
     {
         private readonly DeploymentController m_deploymentController = new("Ubuntu");
-        private readonly WebSocketController _controller = new();
 
         public DeployPage()
         {
             this.InitializeComponent();
             // Set a simple title in the TextBlock defined in XAML
-            _controller.Connected += OnConnected;
-            _controller.Disconnected += OnDisconnected;
-            _controller.LogMessage += OnLogMessage;
-            _controller.StatusReceived += OnStatusReceived;
         }
 
         private async void DeployButton_Click(object sender, RoutedEventArgs e)
@@ -26,48 +21,6 @@ namespace SimulationPlatform.Pages
             var scriptPath = "/mnt/c/Users/swbea/source/repos/CarEmulationProject/Neon/deploy.sh";  // adjust path
             var test = await m_deploymentController.Deploy(scriptPath);
 
-        }
-        private void OnConnected()
-        {
-            _ = DispatcherQueue.TryEnqueue(() =>
-            {
-                ConnectionStatus.Text = "Connected";
-                ConnectionStatus.Foreground = new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Green);
-            });
-        }
-
-        private void OnDisconnected()
-        {
-            _ = DispatcherQueue.TryEnqueue(() =>
-            {
-                ConnectionStatus.Text = "Disconnected";
-                ConnectionStatus.Foreground = new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Red);
-            });
-        }
-
-        private void OnLogMessage(string msg)
-        {
-            _ = DispatcherQueue.TryEnqueue(() =>
-            {
-                LogOutput.Text += $"{DateTime.Now:HH:mm:ss} - {msg}\n";
-            });
-        }
-
-        private void OnStatusReceived(DashboardMessage msg)
-        {
-            _ = DispatcherQueue.TryEnqueue(() =>
-            {
-                StatusText.Text = msg.Status;
-                PidText.Text = msg.Pid.ToString();
-            });
-        }
-        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
-        {
-            await _controller.ConnectAsync("ws://localhost:8080");
-        }
-        private async void DisconnectButton_Click(object sender, RoutedEventArgs e)
-        {
-            await _controller.DisconnectAsync();
         }
     }
 }
--- SimulationPlatform/Pages/LoggingPage.xaml.cs
+++ SimulationPlatform/Pages/LoggingPage.xaml.cs
@@ -1,14 +1,66 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using SimulationPlatform.Controllers;
+using SimulationPlatform.Models;
 
 namespace SimulationPlatform.Pages
 {
     public sealed partial class LoggingPage : Page
     {
+        private readonly WebSocketController _controller = new();
+
         public LoggingPage()
         {
             this.InitializeComponent();
             // Set a simple title in the TextBlock defined in XAML
+            _controller.Connected += OnConnected;
+            _controller.Disconnected += OnDisconnected;
+            _controller.LogMessage += OnLogMessage;
+            _controller.StatusReceived += OnStatusReceived;
         }
 
+        private void OnConnected()
+        {
+            _ = DispatcherQueue.TryEnqueue(() =>
+            {
+                ConnectionStatus.Text = "Connected";
+                ConnectionStatus.Foreground = new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Green);
+            });
+        }
+
+        private void OnDisconnected()
+        {
+            _ = DispatcherQueue.TryEnqueue(() =>
+            {
+                ConnectionStatus.Text = "Disconnected";
+                ConnectionStatus.Foreground = new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Red);
+            });
+        }
+
+        private void OnLogMessage(string msg)
+        {
+            _ = DispatcherQueue.TryEnqueue(() =>
+            {
+                LogOutput.Text += $"{DateTime.Now:HH:mm:ss} - {msg}\n";
+            });
+        }
+
+        private void OnStatusReceived(DashboardMessage msg)
+        {
+            _ = DispatcherQueue.TryEnqueue(() =>
+            {
+                StatusText.Text = msg.Status;
+                PidText.Text = msg.Pid.ToString();
+            });
+        }
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            await _controller.ConnectAsync("ws://localhost:8080");
+        }
+        private async void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            await _controller.DisconnectAsync();
+        }
     }
 }
