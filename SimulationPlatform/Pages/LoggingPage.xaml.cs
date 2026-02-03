7952138275b5e4a48f35faddbe85d2a17bbb1eca
Steven Beardsley <swbeardsley2004@gmail.com>
2026-02-03 22:03:30 +0000

    Tidying up logging page

--- SimulationPlatform/Pages/LoggingPage.xaml.cs
+++ SimulationPlatform/Pages/LoggingPage.xaml.cs
@@ -10,6 +10,7 @@ namespace SimulationPlatform.Pages
     {
         private string m_speed = string.Empty;
         private string m_gear = string.Empty;
+        private string m_rpm = string.Empty;
         private string m_status = string.Empty;
         
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -48,7 +49,19 @@ namespace SimulationPlatform.Pages
             }
         }
 
-        // Property for Status
+        public string Rpm
+        {
+            get => m_rpm;
+            set
+            {
+                if (m_rpm != value)
+                {
+                    m_rpm = value;
+                    OnPropertyChanged(nameof(Rpm));
+                }
+            }
+        }
+
         public string Status
         {
             get => m_status;
@@ -73,10 +86,12 @@ namespace SimulationPlatform.Pages
             {
                 Speed = carData.Speed.ToString();
                 Status = carData.Status.ToString();
+                Rpm = carData.Rpms.ToString();
                 Gear = carData.Gear.ToString();
                 OnPropertyChanged(nameof(Speed));
                 OnPropertyChanged(nameof(Status));
                 OnPropertyChanged(nameof(Gear));
+                OnPropertyChanged(nameof(Rpm));
             });
         }
 
@@ -86,6 +101,7 @@ namespace SimulationPlatform.Pages
             Speed = App.m_model.m_carData.Speed.ToString();
             Status = App.m_model.m_carData.Status.ToString();
             Gear = App.m_model.m_carData.Gear.ToString();
+            Rpm = App.m_model.m_carData.Rpms.ToString();
             App.m_model.m_webSocketController.CarDataReceived += UpdateCarData;
         }
 
